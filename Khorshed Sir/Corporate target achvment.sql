6f63fefad79c6e6f01baf1ba9e5cf759e862a420
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2020-11-17 10:19:17 +0600

    khorshed sir queries modify

--- Khorshed Sir/Corporate target achvment.sql
+++ Khorshed Sir/Corporate target achvment.sql
@@ -1,18 +1,18 @@
 with xyz as ( 
 select h.zid,xsub,d.xprime,--(case when xtrngl='INOP' and xyear='2020' and xper='3' then xqty else 0 end ) xqty, 
 --(case when xtrngl='INOP' and h.xdate between '2020-09-01 06:00:00.000' and '2020-09-17 06:00:00.000' then xprime else 0 end ) saleamount, 
-(case when xtrngl in ('CHQ-','BR--','JVNA','CR--','CRF-','JVI-','JV--') and h.xdate between '2020-11-01' and '2020-11-01' then 
+(case when xtrngl in ('CHQ-','BR--','JVNA','CR--','CRF-','JVI-','JV--') and h.xdate between '2020-11-01' and '2020-11-16' then 
 xprime else 0 end ) collectionamount 
 from glheader h join gldetail d on h.xvoucher=d.xvoucher and h.zid=d.zid join cacus c on d.xsub=c.xcus and d.zid=c.zid 
-where xtrngl<>'OB--' and h.xpostflag='Posted' and c.xsimcardno='Corporate' and h.xdate <='2020-11-01 06:00:00.000') 
+where xtrngl<>'OB--' and h.xpostflag='Posted' and c.xsimcardno='Corporate' and h.xdate <='2020-11-17 06:00:00.000') 
 , cte2 as( 
 select zid,xsub,coalesce((select xsp from cacus where xcus=xyz.xsub),'') xriid, 
 --coalesce((select xqty from opcustarget where xcus=xyz.xsub),0) xqtyst, 
 --coalesce((select xamount from opcustarget where xcus=xyz.xsub),0) xamount, 
 (select sum(xqtychl)/20 from opchallan ch join opchalland cd on ch.zid=cd.zid and ch.xchlnum=cd.xchlnum where ch.zid=xyz.zid and ch.xcus=xyz.xsub and 
-xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-01 06:00:00.000') salesqty, 
+xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-17 06:00:00.000') salesqty, 
 (select sum(xlineamt) from opchallan ch join opchalland cd on ch.zid=cd.zid and ch.xchlnum=cd.xchlnum where ch.zid=xyz.zid and ch.xcus=xyz.xsub and 
-xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-01 06:00:00.000') saleamount, 
+xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-17 06:00:00.000') saleamount, 
 --SUM(xqty) salesqty,SUM(saleamount) saleamount, 
 SUM(collectionamount) collectionamount,SUM(xprime)balance from xyz 
 group by zid,xsub) 
--- Khorshed Sir/Till date sales.sql
+++ Khorshed Sir/Till date sales.sql
@@ -1,23 +1,23 @@
 WITH opsalesdo (zid,xcus,DORemaining, tillsales, ondsales,DARemaining) 
 AS( 
 select h.zid,h.xcus,sum(xqty-xqtychl)/20 DORemaining,0 tillsales,0 ondsales,0 DARemaining from opdor h join opddt 
-d on h.zid=d.zid and h.xdornum=d.xdornum where xstatusdor<>'C-Cancelled' and xdate<='2020-11-08' 
+d on h.zid=d.zid and h.xdornum=d.xdornum where xstatusdor<>'C-Cancelled' and xdate<='2020-11-16' 
 group by h.zid,h.xcus having sum(xqty-xqtychl)>0 
 union all 
 select h.zid,h.xcus,sum(xqtychl/20) DORemaining,0 tillsales,0 ondsales,0 DARemaining from opchallan h join 
 opchalland d on h.zid=d.zid and h.xchlnum=d.xchlnum where xstatuschl='3-Invoiced' and 
-CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-08' group by h.zid,h.xcus 
+CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-16' group by h.zid,h.xcus 
 union all 
-select h.zid,h.xcus,0 DORemaining,sum(xqtychl/20) tillsales,sum(case when CONVERT(date,DATEADD(HOUR,-6,xconfirmt))='2020-11-08' 
+select h.zid,h.xcus,0 DORemaining,sum(xqtychl/20) tillsales,sum(case when CONVERT(date,DATEADD(HOUR,-6,xconfirmt))='2020-11-16' 
 then xqtychl/20 else 0 end) ondsales,0 DARemaining from opchallan h join opchalland d on h.zid=d.zid and h.xchlnum=d.xchlnum 
-where xstatuschl='3-Invoiced' and CONVERT(date,DATEADD(HOUR,-6,xconfirmt)) between '2020-11-01' and '2020-11-08' group by h.zid,h.xcus 
+where xstatuschl='3-Invoiced' and CONVERT(date,DATEADD(HOUR,-6,xconfirmt)) between '2020-11-01' and '2020-11-16' group by h.zid,h.xcus 
 union all 
 select zid,xcus,0 DORemaining,0 tillsales,0 ondsales,sum(xqtysms-xqtychl)/20 from opadvice where 
-xstatusadvmach not in ('5-Voided','1-Open') and CONVERT(date,DATEADD(HOUR,-6,xconfirmt))<='2020-11-08' group by zid,xcus 
+xstatusadvmach not in ('5-Voided','1-Open') and CONVERT(date,DATEADD(HOUR,-6,xconfirmt))<='2020-11-16' group by zid,xcus 
 union all 
 select h.zid,h.xcus,0 DORemaining,0 tillsales,0 ondsales,sum(xqtychl/20) DARemaining from opchallan h join opchalland d 
-on h.zid=d.zid and h.xchlnum=d.xchlnum where xstatuschl='3-Invoiced' and CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-08' 
-and xadvnum not in (select xadvnum from opadvice where CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-08') group by h.zid,h.xcus 
+on h.zid=d.zid and h.xchlnum=d.xchlnum where xstatuschl='3-Invoiced' and CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-16' 
+and xadvnum not in (select xadvnum from opadvice where CONVERT(date,DATEADD(HOUR,-6,xconfirmt))>'2020-11-16') group by h.zid,h.xcus 
 ) 
 select xsimcardno,case when xsimcardno='Corporate' then xspouse else xbloodgrp end xbloodgrp, sum(DORemaining) DORemaining, 
 sum(tillsales) tillsales, sum(ondsales) ondsales,sum(DARemaining) DARemaining from opsalesdo t join cacus c on t.zid=c.zid 
