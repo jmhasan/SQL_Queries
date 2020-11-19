c44568eda2f3d4f87cd932d9b91c53b7d32fdb21
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2020-11-19 10:19:25 +0600

    Variable passing inside queries

--- Khorshed Sir/AI wise target achv.sql
+++ Khorshed Sir/AI wise target achv.sql
@@ -1,14 +1,23 @@
+DECLARE @fxdate AS date
+DECLARE @txdate AS date
+DECLARE @xyear as int
+DECLARE @xper as int
+SET @fxdate = '2020-11-01'
+SET @txdate = '2020-11-18'
+set @xyear=2020
+set @xper=11;
+
 with cte as 
-(select xdiv,xzone,xteam xemp,(select xempnew from opritargetdt where xyear=2020 and xper=11 and xemp=opchallandt.xteam) xempnew, 
-COUNT(distinct xrow) xrow,(select xqty from opritargetdt where xyear=2020 and xper=11 and xemp=opchallandt.xteam) xqty, 
-SUM(xqtychl)/20 xqtychl from opchallandt where xdatecom between '2020-11-01' and '2020-11-17' 
+(select xdiv,xzone,xteam xemp,(select xempnew from opritargetdt where xyear=@xyear and xper=@xper and xemp=opchallandt.xteam) xempnew, 
+COUNT(distinct xrow) xrow,(select xqty from opritargetdt where xyear=@xyear and xper=@xper and xemp=opchallandt.xteam) xqty, 
+SUM(xqtychl)/20 xqtychl from opchallandt where xdatecom between @fxdate and @txdate 
 and xdiv in ('Dhaka','Out Dhaka','Corporate') 
 --and xzone in ('MYMENSINGH','NILPHAMARI') 
 --and xrow not in (select xrow from opchallandt where xdatecom between '2020-09-02' and '2020-09-13' and xdiv in ('Dhaka','Out Dhaka') ) 
 group by xdiv,xzone,xteam 
 union all 
 select xsimcardno xdiv,xzone,xemp,xempnew,0 xrow,xqty,0 xqtychl from opritargetdt where xyear=2020 and xper=11 and 
-xemp not in (select xteam from opchallan where xdatecom between '2020-11-01' and '2020-11-17' and xdiv in ('Dhaka','Out Dhaka','Corporate') )) 
+xemp not in (select xteam from opchallan where xdatecom between @fxdate and @txdate and xdiv in ('Dhaka','Out Dhaka','Corporate') )) 
 select xdiv Division,xzone Zone,xempnew AI_Code, 
 (select xname from prmst where xemp=cte.xempnew and zid=100000) AI_Name, 
 (select xmobile1 from prmst where xemp=cte.xempnew and zid=100000) AI_Mobile, 
--- Khorshed Sir/Corporate target achvment.sql
+++ Khorshed Sir/Corporate target achvment.sql
@@ -1,23 +1,32 @@
+DECLARE @fxdate AS date
+DECLARE @txdate AS date
+DECLARE @xyear as int
+DECLARE @xper as int
+SET @fxdate = '2020-11-01'
+SET @txdate = '2020-11-18'
+set @xyear=2020
+set @xper=11;
+
 with xyz as ( 
 select h.zid,xsub,d.xprime,--(case when xtrngl='INOP' and xyear='2020' and xper='3' then xqty else 0 end ) xqty, 
 --(case when xtrngl='INOP' and h.xdate between '2020-09-01 06:00:00.000' and '2020-09-17 06:00:00.000' then xprime else 0 end ) saleamount, 
-(case when xtrngl in ('CHQ-','BR--','JVNA','CR--','CRF-','JVI-','JV--') and h.xdate between '2020-11-01' and '2020-11-17' then 
+(case when xtrngl in ('CHQ-','BR--','JVNA','CR--','CRF-','JVI-','JV--') and h.xdate between @fxdate and @txdate then 
 xprime else 0 end ) collectionamount 
 from glheader h join gldetail d on h.xvoucher=d.xvoucher and h.zid=d.zid join cacus c on d.xsub=c.xcus and d.zid=c.zid 
-where xtrngl<>'OB--' and h.xpostflag='Posted' and c.xsimcardno='Corporate' and h.xdate <='2020-11-18 06:00:00.000') 
+where xtrngl<>'OB--' and h.xpostflag='Posted' and c.xsimcardno='Corporate' and CONVERT(date,DATEADD(HOUR,-6, h.xdate)) <=@txdate) 
 , cte2 as( 
 select zid,xsub,coalesce((select xsp from cacus where xcus=xyz.xsub),'') xriid, 
 --coalesce((select xqty from opcustarget where xcus=xyz.xsub),0) xqtyst, 
 --coalesce((select xamount from opcustarget where xcus=xyz.xsub),0) xamount, 
 (select sum(xqtychl)/20 from opchallan ch join opchalland cd on ch.zid=cd.zid and ch.xchlnum=cd.xchlnum where ch.zid=xyz.zid and ch.xcus=xyz.xsub and 
-xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-18 06:00:00.000') salesqty, 
+CONVERT(date,DATEADD(HOUR,-6,xconfirmt)) between @fxdate and @txdate) salesqty, 
 (select sum(xlineamt) from opchallan ch join opchalland cd on ch.zid=cd.zid and ch.xchlnum=cd.xchlnum where ch.zid=xyz.zid and ch.xcus=xyz.xsub and 
-xconfirmt between '2020-11-01 06:00:00.000' and '2020-11-18 06:00:00.000') saleamount, 
+CONVERT(date,DATEADD(HOUR,-6,xconfirmt)) between @fxdate and @txdate) saleamount, 
 --SUM(xqty) salesqty,SUM(saleamount) saleamount, 
 SUM(collectionamount) collectionamount,SUM(xprime)balance from xyz 
 group by zid,xsub) 
 select xriid RI_Code,(select xname from prmst where zid=100000 and xemp=cte2.xriid) RI_Name , 
-coalesce((select xqty from opritargetdt where xyear=2020 and xper=11 and xemp=cte2.xriid and xemp>''),0) SalesTarget, 
+coalesce((select xqty from opritargetdt where xyear=@xyear and xper=@xper and xemp=cte2.xriid and xemp>''),0) SalesTarget, 
 SUM(salesqty) salesqty,SUM(saleamount) saleamount,coalesce((select xamount from opritargetdt where xyear=2020 and xper=11 and xemp=cte2.xriid and xemp>''),0) SalesTarget, 
 SUM(collectionamount) collectionamount, 
 SUM(balance) balance,(SUM(balance)/SUM(saleamount))*30 DSO from cte2 
