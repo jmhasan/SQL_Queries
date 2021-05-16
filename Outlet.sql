f8e20c1e6a86f8ef10521bbc0f1cfc1d075d22b7
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2021-05-16 14:40:49 +0600

    matching header detail and outlet

--- Outlet.sql
+++ Outlet.sql
@@ -1,2 +1,3 @@
-select ztime,xzone,xcus,dbo.cusname(xcus),xrow,xid,xoutletname,xriid,xziauth from 
-caoutlet where xcus='CUS-003472' order by ztime desc
+select ztime,xzone,xcus,dbo.cusname(xcus),xrow,xid,xoutletname,xriid,(select xname from prmst where xemp=caoutlet.xriid) xname,xziauth from 
+caoutlet where xcus='CUS-001744' order by ztime desc
+
