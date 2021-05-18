fd41fe3a5b40e99e9ad20d2926a9d78db4336218
Jm Hasan <jm_hasan@yahoo.com>
2021-05-18 09:58:00 +0600

    18-05-2021

--- Khorshed Sir/Corporate target achvment.sql
+++ Khorshed Sir/Corporate target achvment.sql
@@ -3,7 +3,7 @@ DECLARE @txdate AS date
 DECLARE @xyear as int
 DECLARE @xper as int
 SET @fxdate = '2021-05-01'
-SET @txdate = '2021-05-16'
+SET @txdate = '2021-05-17'
 set @xyear=2021
 set @xper=05;
 
