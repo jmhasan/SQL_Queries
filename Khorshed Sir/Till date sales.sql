401d0049ed9dfe2a17a69fc6f350bd469beb7e69
Jm Hasan <jm_hasan@yahoo.com>
2021-05-20 09:18:44 +0600

    zone and district wise sales

--- Khorshed Sir/Till date sales.sql
+++ Khorshed Sir/Till date sales.sql
@@ -1,7 +1,7 @@
 DECLARE @fxdate AS date
 DECLARE @txdate AS date
 SET @fxdate = '2021-05-01'
-SET @txdate = '2021-05-17';
+SET @txdate = '2021-05-18';
 
 WITH opsalesdo (zid,xcus,DORemaining, tillsales, ondsales,DARemaining) 
 AS( 
--- Outlet.sql
+++ Outlet.sql
@@ -1,4 +1,4 @@
 select ztime,xzone,xcus,dbo.cusname(xcus) Customer_Name,xrow,xid,xoutletname,xriid,dbo.empname(xriid) RI_Name,xziauth from 
-caoutlet where xid ='TNG-00039' in ('MLV-00025','MLV-00023') --xcus='CUS-005363' and xoutletname like '%jisan%' order by ztime desc
+caoutlet where  xcus='CUS-000352' order by ztime desc
 
 select * from opadviceapp where xid='HBG-02869' --002061
