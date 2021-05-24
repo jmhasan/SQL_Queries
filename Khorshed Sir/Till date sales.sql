8cdfbf7ca4b9c8d27b50f6cb38b5e06036efbb63
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2021-05-24 13:25:24 +0600

    day wise void and last first challan date

--- Khorshed Sir/Till date sales.sql
+++ Khorshed Sir/Till date sales.sql
@@ -1,7 +1,7 @@
 DECLARE @fxdate AS date
 DECLARE @txdate AS date
 SET @fxdate = '2021-05-01'
-SET @txdate = '2021-05-19';
+SET @txdate = '2021-05-23';
 
 WITH opsalesdo (zid,xcus,DORemaining, tillsales, ondsales,DARemaining) 
 AS( 
