dd3a0409636df820543bb5c3090f2a96ceda770e
Jm Hasan <jm_hasan@yahoo.com>
2021-08-16 19:43:36 +0600

    Rete update query modify

--- Rate Update.sql
+++ Rate Update.sql
@@ -27,3 +27,16 @@ xcus in (select xcus from cacus where xsimcardno='Chittagong')
 
 --update cacuscorrt set xnetrate=xnetrate+10,zutime=GETDATE(),xrem=xnetrate  where 
 --xcus in (select xcus from cacus where xsimcardno='Chittagong'),
+
+
+
+
+----Last Update query 
+select ztime,zutime,xcus,xwh,isincentive,xdesc,xitem,xqtyord,xrate,xrate-10,xchgdel,xlineamt,xqtyord*(xrate-10),xamt,xqtyord*((xrate-10)-xchgdel),xziauth from oporderapp where
+ xziauth=1 and xdate='2021-08-16' and xcus in (select xcus from cacus where xsimcardno in ('Dhaka', 'Out Dhaka') ) 
+and xtrnnum not in (select xtrnnum from opordonline ) and ztime>='2021-08-16 13:24:29.180'
+
+
+
+--update oporderapp set xrate=xrate-10,xlineamt=xqtyord*(xrate-10),xamt=xqtyord*((xrate-10)-xchgdel) where xziauth=1 and xdate='2021-08-16' and xcus in (select xcus from cacus where xsimcardno in ('Dhaka', 'Out Dhaka') ) 
+--and xtrnnum not in (select xtrnnum from opordonline ) and ztime>='2021-08-16 13:24:29.180',
